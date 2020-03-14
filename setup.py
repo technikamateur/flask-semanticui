6ac3a6e83421271030b42adc5e4b96218b345fd0
Daniel <daniel@technikamateur.de>
2020-03-14 16:17:01 +0100

    init

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='Flask-Semantic-UI',
+    version='1.0',
+    url='https://github.com/technikamateur/flask-semanticui',
+    author='Daniel aka technikamateur',
+    author_email='daniel@technikamateur.de',
+    description='An simple extension that includes Semantic UI in your project',
+    packages=['flask_semanticui'],
+    include_package_data=True,
+    install_requires=[
+        'Flask>=1.0',
+        'flask_wtf>=0.14'
+    ]
+)
